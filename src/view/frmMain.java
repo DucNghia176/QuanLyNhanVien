bbf6354efe4b439b190199eaff24cacf59e7e874
DucNghia176 <nghi7a@gmail.com>
2024-11-28 23:47:59 +0700

    nghiệp vụ

--- src/view/frmMain.java
+++ src/view/frmMain.java
@@ -34,11 +34,11 @@ public class frmMain extends javax.swing.JFrame {
                 break;
             case 2: // User
                 // Ẩn các chức năng dành cho admin
-                
+
                 break;
             case 3: // Manager
                 // Ẩn một số tính năng không liên quan
-                btUser.setVisible(false);
+                btHome.setVisible(false);
                 btDerpartment.setVisible(false);
                 break;
             default:
@@ -66,16 +66,21 @@ public class frmMain extends javax.swing.JFrame {
         }
     }
 
-    private void runForm(javax.swing.JInternalFrame form) {
+    private void runForm(javax.swing.JInternalFrame form, javax.swing.JButton button) {
+        resetButtonColors();
+        button.setBackground(new java.awt.Color(128, 0, 128));
         closeAllInternalFrames(); // Đóng tất cả các form hiện tại
-//        form.setSize(myDesktop.getWidth(), myDesktop.getHeight()); // Thiết lập kích thước toàn màn hình?
         myDesktop.add(form); // Thêm form vào JDesktopPane
         form.setVisible(true); // Hiển thị form
-//        try {
-//            form.setMaximum(true); // Phóng to toàn màn hình
-//        } catch (java.beans.PropertyVetoException ex) {
-//            ex.printStackTrace(); // In lỗi nếu có
-//        }
+    }
+
+    private void resetButtonColors() {
+        // Đặt lại màu nền mặc định cho tất cả các nút
+        btHome.setBackground(new java.awt.Color(76, 175, 80));
+        btEmployess.setBackground(new java.awt.Color(76, 175, 80));
+        btUser.setBackground(new java.awt.Color(76, 175, 80));
+        btDerpartment.setBackground(new java.awt.Color(76, 175, 80));
+        btPosition.setBackground(new java.awt.Color(76, 175, 80));
     }
 
     public frmMain() {
@@ -92,41 +97,104 @@ public class frmMain extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        btDerpartment = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        btHome = new javax.swing.JButton();
         btEmployess = new javax.swing.JButton();
         btUser = new javax.swing.JButton();
-        jButton1 = new javax.swing.JButton();
+        btDerpartment = new javax.swing.JButton();
+        btPosition = new javax.swing.JButton();
         myDesktop = new javax.swing.JDesktopPane();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(0, 153, 153));
+        jPanel1.setBackground(new java.awt.Color(82, 83, 81));
 
-        btDerpartment.setText("Derpartment");
-        btDerpartment.addActionListener(new java.awt.event.ActionListener() {
+        jPanel2.setBackground(new java.awt.Color(255, 0, 0));
+        jPanel2.setForeground(new java.awt.Color(255, 0, 0));
+
+        jLabel2.setBackground(new java.awt.Color(232, 64, 60));
+        jLabel2.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icon/tq.png"))); // NOI18N
+        jLabel2.setText("QUẢN LÍ NHÂN VIÊN");
+        jLabel2.setIconTextGap(3);
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 290, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(15, 15, 15))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(10, 10, 10)
+                .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(13, 13, 13))
+        );
+
+        btHome.setBackground(new java.awt.Color(76, 175, 80));
+        btHome.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        btHome.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icon/home.png"))); // NOI18N
+        btHome.setText("Màn Hình Chính");
+        btHome.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        btHome.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        btHome.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btDerpartmentActionPerformed(evt);
+                btHomeActionPerformed(evt);
             }
         });
 
-        btEmployess.setText("Employess");
+        btEmployess.setBackground(new java.awt.Color(76, 175, 80));
+        btEmployess.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        btEmployess.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icon/employ.png"))); // NOI18N
+        btEmployess.setText("Employee Management ");
+        btEmployess.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        btEmployess.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
         btEmployess.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btEmployessActionPerformed(evt);
             }
         });
 
-        btUser.setText("User");
+        btUser.setBackground(new java.awt.Color(76, 175, 80));
+        btUser.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        btUser.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icon/user.png"))); // NOI18N
+        btUser.setText("Account Management");
+        btUser.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        btUser.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
         btUser.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btUserActionPerformed(evt);
             }
         });
 
-        jButton1.setText("Position");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btDerpartment.setBackground(new java.awt.Color(76, 175, 80));
+        btDerpartment.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        btDerpartment.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icon/derp.png"))); // NOI18N
+        btDerpartment.setText("Derpartment Management ");
+        btDerpartment.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        btDerpartment.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        btDerpartment.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btDerpartmentActionPerformed(evt);
+            }
+        });
+
+        btPosition.setBackground(new java.awt.Color(76, 175, 80));
+        btPosition.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        btPosition.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icon/pos.png"))); // NOI18N
+        btPosition.setText("Position Management ");
+        btPosition.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        btPosition.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        btPosition.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btPositionActionPerformed(evt);
             }
         });
 
@@ -134,30 +202,41 @@ public class frmMain extends javax.swing.JFrame {
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(btUser, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(btEmployess, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(btDerpartment, javax.swing.GroupLayout.DEFAULT_SIZE, 141, Short.MAX_VALUE)
-            .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(btDerpartment, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btPosition, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btUser, javax.swing.GroupLayout.PREFERRED_SIZE, 263, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addComponent(btHome, javax.swing.GroupLayout.PREFERRED_SIZE, 261, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btEmployess, javax.swing.GroupLayout.PREFERRED_SIZE, 263, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(17, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(10, 10, 10)
+                .addComponent(btHome, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(20, 20, 20)
-                .addComponent(btUser, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(btDerpartment, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(btEmployess, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 176, Short.MAX_VALUE))
+                .addComponent(btEmployess, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(20, 20, 20)
+                .addComponent(btUser, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(20, 20, 20)
+                .addComponent(btDerpartment, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(20, 20, 20)
+                .addComponent(btPosition, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(98, 98, 98))
         );
 
         javax.swing.GroupLayout myDesktopLayout = new javax.swing.GroupLayout(myDesktop);
         myDesktop.setLayout(myDesktopLayout);
         myDesktopLayout.setHorizontalGroup(
             myDesktopLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 838, Short.MAX_VALUE)
+            .addGap(0, 466, Short.MAX_VALUE)
         );
         myDesktopLayout.setVerticalGroup(
             myDesktopLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -171,7 +250,8 @@ public class frmMain extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(myDesktop))
+                .addComponent(myDesktop)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -184,22 +264,26 @@ public class frmMain extends javax.swing.JFrame {
 
     private void btEmployessActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btEmployessActionPerformed
         // TODO add your handling code here:
-        runForm(new frmEmployees());
+        runForm(new frmEmployees(),btEmployess);
     }//GEN-LAST:event_btEmployessActionPerformed
 
     private void btDerpartmentActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btDerpartmentActionPerformed
         // TODO add your handling code here:
-        runForm(new frmDepartment());
+        runForm(new frmDepartment(),btDerpartment);
     }//GEN-LAST:event_btDerpartmentActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btPositionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btPositionActionPerformed
         // TODO add your handling code here:
-        runForm(new frmPosition());
-    }//GEN-LAST:event_jButton1ActionPerformed
+        runForm(new frmPosition(),btPosition);
+    }//GEN-LAST:event_btPositionActionPerformed
+
+    private void btHomeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btHomeActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btHomeActionPerformed
 
     private void btUserActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btUserActionPerformed
         // TODO add your handling code here:
-        runForm(new frmUser());
+        runForm(new frmUser(),btUser);
     }//GEN-LAST:event_btUserActionPerformed
 
     /**
@@ -243,9 +327,12 @@ public class frmMain extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btDerpartment;
     private javax.swing.JButton btEmployess;
+    private javax.swing.JButton btHome;
+    private javax.swing.JButton btPosition;
     private javax.swing.JButton btUser;
-    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JDesktopPane myDesktop;
     // End of variables declaration//GEN-END:variables
 }
